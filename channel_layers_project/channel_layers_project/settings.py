f228c785096eb2ab09f59bd333fc1efcdfee3748
abhaymaurya57 <abhaymaurya54321@gmail.com>
2026-06-10 15:13:01 +0530

    google auth change

--- channel_layers_project/channel_layers_project/settings.py
+++ channel_layers_project/channel_layers_project/settings.py
@@ -76,6 +76,20 @@ CSRF_COOKIE_SECURE = True
 # Ye existing Django user aur Google account ko email ke basis par link karne me help karta hai.
 SOCIAL_AUTH_ASSOCIATE_BY_EMAIL = True
 
+# Ye add karo
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.social_auth.associate_by_email',  # ← key line
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
+
 SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE = ['email', 'profile']
 SOCIAL_AUTH_LOGIN_REDIRECT_URL = '/get-jwt-token-after-google-login/'
 SOCIAL_AUTH_URL_NAMESPACE = "social"
