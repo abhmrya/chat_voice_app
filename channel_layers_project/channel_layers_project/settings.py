58cbdcfafeb74edbdab9701fb8a78dc566be8e7c
abhaymaurya57 <abhaymaurya54321@gmail.com>
2026-06-10 15:30:30 +0530

    jwt time 5 min

--- channel_layers_project/channel_layers_project/settings.py
+++ channel_layers_project/channel_layers_project/settings.py
@@ -263,7 +263,7 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": False,  # ✅ disable blacklist
